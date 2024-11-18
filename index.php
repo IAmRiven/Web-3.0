7390584456ebcc0e628ca564b2051c28f3c46539
Antojo02 <1406117@alu.murciaeduca.es>
2024-11-18 12:01:07 +0100

    bgverde

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de Inicio de Sesión</title>
+</head>
+<body>
+    <?php
+        $num = 2;
+        $nombre = "Antonio José Tortajada Palazón";
+        echo "Hola Mundo soy $nombre <br>";
+        echo "El número es: $num ";
+    ?>
+</body>
+</html>
